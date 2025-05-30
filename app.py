cf7989004a0d952713a5deb66c87e2d43fa82df7
Harryl35 <HArryl29@nycstudents.net>
2025-05-30 00:49:00 -0400

    a

--- app.py
+++ app.py
@@ -7,19 +7,14 @@ DIGIMON_API_URL = "https://digimon-api.vercel.app/api/digimon"
 
 @app.route('/')
 def home():
-    response = requests.get(DIGIMON_API_URL)
-    digimon_list = response.json()
-    return render_template("index.html", digimon_list=digimon_list)
-
-@app.route('/search')
-def search_page():
     return render_template("search.html")
 
 @app.route('/search_digimon')
 def search_digimon():
-    query = request.args.get('query', '').capitalize()
+    query = request.args.get('query', '').capitalize()  # Capitalizing for consistency
     response = requests.get(f"{DIGIMON_API_URL}/name/{query}")
     digimon_data = response.json()
+
     return jsonify(digimon_data[0] if digimon_data else {"error": "No Digimon found"})
 
 if __name__ == '__main__':
